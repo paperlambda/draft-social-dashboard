f09d71892ea3e405e675fac3a432f0c53a7aa3eb
paperlambda <revzankamarullah@gmail.com>
2019-08-16 17:32:08 +0700

    -Add readme -Update navbar ui

---
+++ README.md
@@ -0,0 +1,16 @@
+## DRAFT
+
+Social media dashboard powered by https://jsonplaceholder.typicode.com/
+
+### How to run
+
+```
+npm install
+npm start
+```
+
+### Deployment build
+
+```
+npm build
+```
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 import Container from '@/components/Container'
+import Text from '@/components/Text'
+import Flex from '@/components/Flex'
 
 const Navbar = () => {
   return (
     <Root>
       <Container>
-        <h3>Draft</h3>
+        <Nav jc="flex-start">
+          <Text variant="title" color="#ffffff" bold>
+            DRAFT
+          </Text>
+        </Nav>
       </Container>
     </Root>
   )
@@ -21,6 +27,13 @@ const Root = styled('div')`
   background: ${props => props.theme.color.blue};
   color: #ffffff;
   box-shadow: 0px 0px 3px 0px #cccccc;
+`
+
+const Nav = styled(Flex)`
+  height: 100%;
+  > div {
+    margin: 0;
+  }
 `
 
 export default Navbar
